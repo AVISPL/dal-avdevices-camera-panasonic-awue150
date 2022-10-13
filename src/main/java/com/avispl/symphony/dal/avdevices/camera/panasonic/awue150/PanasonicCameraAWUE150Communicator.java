c48ca74d0be5585e3bad012925054c2631fb10fb
HieuLM-AVISPL <hm15015@avispl.com>
2022-10-13 21:26:16 +0700

    draft adapter

---
+++ src/main/java/com/avispl/symphony/dal/avdevices/camera/panasonic/awue150/PanasonicCameraAWUE150Communicator.java
@@ -0,0 +1,51 @@
+package com.avispl.symphony.dal.avdevices.camera.panasonic.awue150;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.avispl.symphony.api.dal.control.Controller;
+import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
+import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
+import com.avispl.symphony.api.dal.dto.monitor.Statistics;
+import com.avispl.symphony.api.dal.monitor.Monitorable;
+import com.avispl.symphony.dal.communicator.RestCommunicator;
+
+/**
+ * PanasonicCameraAWUE150Communicator
+ *
+ * @author Harry / Symphony Dev Team<br>
+ * Created on 11/13/2022
+ * @since 1.0.0
+ */
+public class PanasonicCameraAWUE150Communicator extends RestCommunicator implements  Monitorable, Controller {
+
+	@Override
+	public List<Statistics> getMultipleStatistics() throws Exception {
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("Getting statistics from the device Panasonic camera UE150 at host %s with port %s", this.host, this.getPort()));
+		}
+		final ExtendedStatistics extendedStatistics = new ExtendedStatistics();
+		final Map<String, String> stats = new HashMap<>();
+		stats.put("property", "Value");
+		extendedStatistics.setStatistics(stats);
+
+		return Collections.singletonList(extendedStatistics);
+	}
+
+	@Override
+	public void controlProperty(ControllableProperty controllableProperty) throws Exception {
+
+	}
+
+	@Override
+	public void controlProperties(List<ControllableProperty> list) throws Exception {
+
+	}
+
+	@Override
+	protected void authenticate() throws Exception {
+
+	}
+}
